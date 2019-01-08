fbf53a785bfbd5904e4ff5331c6c6a45ab168420
brandypoiry <brandypoiry@gmail.com>
2019-01-07 22:23:34 -0600

    [Exercise]

--- 5.2.py
+++ 5.2.py
@@ -1,14 +1,20 @@
 largest = None
 smallest = None
 
-try:
-    while True:
-        num = int(input("Enter a number: "))
-        largest = max(num)
-        smallest = min(num)
-        if num == "done" : break
-except:
-    print("Invalid Input")
+while True:
+    try:
+        num = input("Enter a number: ")
+        if num == "done":
+            break
+        num = int(num)
+        if largest is None or largest < num:
+            largest = num
+        if smallest is None or smallest > num:
+            smallest = num
+    except:
+        print("Invalid Input")
+
+
 
 print("Maximum:", largest)
 print("Minimum:", smallest)
